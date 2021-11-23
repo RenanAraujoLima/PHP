8c6251632e779eafc9d9c9658d7c40e8a7f9714c
Renan Araujo <rlima@integranaweb.com.br>
2021-11-23 13:00:36 -0300

    Versão com categoria na mensagem

--- index.php
+++ index.php
@@ -8,37 +8,25 @@ $categorias[] = 'adulto';
 //print_r($categorias);
 
 $nome = 'João';
-$idade = 18;
+$idade = 8;
 
 //var_dump($nome);
 //var_dump($idade);
 
 if ($idade >= 6 && $idade <= 12)
 { 
-    for ($i = 0; $i <= count($categorias); $i++)
-        {
-            if ($categorias[$i] == 'infantil')
-            echo "O nadador " .$nome. " compete na categoria " .$categorias[$i];
-        }
+    echo "O nadador " .$nome. " compete na categoria infantil";
 }
 
 else if ($idade >= 13 && $idade <= 18)
 {
-    for ($i = 0; $i <= count($categorias); $i++)
-        {
-            if ($categorias[$i] == 'adolescente')
-            echo "O nadador " . $nome. " compete na categoria " .$categorias[$i];
-        }
+    echo "O nadador " . $nome. " compete na categoria adolescente";
 }
 
-
 else
-{
-    for ($i = 0; $i <= count($categorias); $i++)
-        {
-            if ($categorias[$i] == 'adulto')
-            echo "O nadador " .$nome. " compete na categoria adulto " .$categorias[$i];
-        }
+{  
+    if ($categorias[$i] == 'adulto')
+    echo "O nadador " .$nome. " compete na categoria adulto";    
 }
 
 ?>
